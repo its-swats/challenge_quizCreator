f7e0007455d06b569ca631f1c14fa2acaf41a316
Shawn Watson <shawnw86@gmail.com>
2016-05-05 11:41:48 -0700

    Add spec file, but not enough time left to hook it up to database

---
+++ spec/quiz_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+
+
+describe Quiz do
+  describe('#initialize') do
+  	describe('creates a quiz object with the correct number of questions') do
+  		#need to hook up spec to a fake database
+  	end
+  end
+end
