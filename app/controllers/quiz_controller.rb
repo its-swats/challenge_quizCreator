ed75ff81dadf5cad1184435c8386345de083d8c3
Shawn Watson <shawnw86@gmail.com>
2016-05-05 11:31:55 -0700

    Add radio buttons for sorting by difficulty and strand

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -1,6 +1,6 @@
 get '/quiz' do
 	if params['questions'].to_i > 0
-		@questions = Quiz.new(params['questions'].to_i)
+		@questions = Quiz.new(params['questions'].to_i, params['sort'])
 	else
 		@errors = "Quizzes must contain at least one question."
 	end
